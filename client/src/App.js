c9c4e240293fef38f8bae58749568bb53815f93b
CorysPC <corystory2@gmail.com>
2022-11-17 18:53:03 -0700

    changed front welcome page to login

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,11 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Switch>
+        {/* <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        </Switch> */}
       </>
     </Router>
   );
