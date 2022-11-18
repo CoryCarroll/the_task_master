c583d7ec33818d8fa11ded8954a951c7f642431f
PeachDoors <taylorpterrill@gmail.com>
2022-11-17 19:38:01 -0700

    updated task model

---
+++ server/models/Task.js
@@ -0,0 +1,31 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  // TODO add creator field for multiple users
+  // TODO add assignedTo 
+  // TODO check taskId
+  taskId: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  deadline: {
+    // TODO change to date if we implement calendar
+    type: String,
+    requied: false,
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+});
+
+module.exports = taskSchema;
