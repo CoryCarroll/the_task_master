e17bbd9502f77e56db925d97d84ce179488750c4
PeachDoors <taylorpterrill@gmail.com>
2022-11-21 19:34:26 -0700

    updated routes, controller, and front end API to make sure function and variable names are correct

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,12 +1,8 @@
-// TODO update saveBook, deleteBook, book route
-
 const router = require('express').Router();
 const {
-  createUser,
   getSingleUser,
-  //update saveBook
+  createUser,
   saveTask,
-  //update
   deleteTask,
   login,
 } = require('../../controllers/user-controller');
@@ -15,14 +11,12 @@ const {
 const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
-//update saveBook
 router.route('/').post(createUser).put(authMiddleware, saveTask);
 
 router.route('/login').post(login);
 
 router.route('/me').get(authMiddleware, getSingleUser);
 
-//update deleteBook, book route
 router.route('/tasks/:taskId').delete(authMiddleware, deleteTask);
 // TODO check routes if necessary
 
