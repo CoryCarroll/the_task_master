2f2371a5bebf4f26b47899c738ba762f44ac738c
Cory Carroll <corystory2@gmail.com>
2024-02-11 14:40:57 -0700

    IT ALL WORKS

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/task_master', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/task_master');
 
 module.exports = mongoose.connection;
