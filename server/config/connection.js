e6085fe65a891f12bb6d874d96680c7d379730c4
Cory Carroll <corystory2@gmail.com>
2024-02-11 21:57:21 -0700

    changes to mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+const MONGODB_URI = process.env.MONGODB_URI;
 
-module.exports = mongoose.connection;
+if (!MONGODB_URI) {
+  console.error('MongoDB URI is missing. Set the MONGODB_URI environment variable.');
+  process.exit(1); // Exit the process if MongoDB URI is not set
+}
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+  process.exit(1); // Exit the process on connection error
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
