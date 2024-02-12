450d41f2484a4c1249b22f5b3a2565ad6edd7c0f
Cory Carroll <corystory2@gmail.com>
2024-02-11 18:36:59 -0700

    added code to server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
 db.once('open', () => {
